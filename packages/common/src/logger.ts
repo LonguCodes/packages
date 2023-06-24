afa6c6ba8034ca95c16828c1ed6ff663a2fa19e0
ci <ci@ci.com>
2023-06-24 14:00:30 +0200

    fix(common): fixed usage of chalk

--- packages/common/src/logger.ts
+++ packages/common/src/logger.ts
@@ -1,18 +1,18 @@
-import * as chalk from 'chalk';
+import { blue, yellow, red } from 'chalk';
 
 export class Logger {
   public static log(...args: any[]) {
     console.log(...args);
   }
   public static info(...parts: any[]) {
-    this.log(chalk.blue('[INFO]'), ...parts);
+    this.log(blue('[INFO]'), ...parts);
   }
 
   public static warn(...parts: any[]) {
-    this.log(chalk.yellow('[WARN]'), ...parts);
+    this.log(yellow('[WARN]'), ...parts);
   }
 
   public static error(...parts: any[]) {
-    this.log(chalk.red('[ERROR]'), ...parts);
+    this.log(red('[ERROR]'), ...parts);
   }
 }
